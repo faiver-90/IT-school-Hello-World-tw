c04a65ca818cf0aae2110fc0754b3df5e9905c5e
faiver90 <faiver90@gmail.com>
2025-11-19 10:55:27 +0200

    'add queue'

--- src/lesson/views.py
+++ src/lesson/views.py
@@ -18,6 +18,9 @@ class LessonRetrieveUpdateDeleteView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Lesson.objects.select_related("student").all()
     serializer_class = LessonSerializer
 
-    def perform_destroy(self, serializer):
-        lesson = serializer.save()
-        process_lesson(lesson)
+    def perform_update(self, serializer):
+        old_lesson = self.get_object()
+        new_lesson = serializer.save()
+
+        if old_lesson.end != new_lesson.end:
+            process_lesson(new_lesson)
